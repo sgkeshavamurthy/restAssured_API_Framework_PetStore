c086a943e4c0236ed7b4d1938e16fc9445675168
Keshav <sgkeshavamurthy@gmail.com>
2026-02-21 14:30:26 +0530

    added class file for extent file from tutorial

--- src/test/java/api/testcases/User_DDTestCases.java
+++ src/test/java/api/testcases/User_DDTestCases.java
@@ -3,6 +3,7 @@ package api.testcases;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import api.endpoints.Endpoints;
 import api.endpoints.Routes;
 import api.payloads.createUser_Payload;
 import api.utilities.DataLoader;
@@ -14,7 +15,7 @@ public class User_DDTestCases {
 	
 	createUser_Payload CUP = new createUser_Payload();
 	@Test(priority = 1, dataProvider = "dataForPOST", dataProviderClass=DataLoader.class)
-	public void load_Data(String id, String username, String fname, String lname,  String email, String pwd, String phno)
+	public void test_POSTrequest(String id, String username, String fname, String lname,  String email, String pwd, String phno)
 	{
 		
 		CUP.setFirstName(fname);
@@ -25,41 +26,10 @@ public class User_DDTestCases {
 		CUP.setPassword(pwd);
 		CUP.setPhone(phno);
 		CUP.setUserStatus(0);
+		Response response = Endpoints.create_user(CUP);
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
 		
 	}
-	@Test (priority = 2)
-	public void POSTRequest()
-	{
-		Response response =
-		RestAssured
-			.given()
-				.contentType(ContentType.JSON)
-				.body(CUP)
-				.accept(ContentType.JSON)
-			.when()
-				.post(Routes.post_url);
-		response.then().log().all();
-		Assert.assertEquals(response.getStatusCode(),200);
-	}
 	
-	
-//	public void test_CreateUser()	
-//	{
-//		String filename = "C:\\Users\\hp\\eclipse-workspace\\RestAssuredFramework-17Feb\\ArtifactID_RestAssuredProject\\src\\test\\java\\api\\testdata\\TestData.xlsx";
-//		DataLoader DM = new DataLoader();
-//		
-//		String returnVal[][] = DM.DataMatrix(filename, "Sheet1");
-//		int rows = returnVal.length;
-//		int columns = returnVal[0].length;
-//		System.out.println("Rows: " + rows + "  " + "Columns: " + columns);
-//		for(int i=0;i<rows-1; i++)
-//		{
-//			for(int j=0; j<columns; j++)
-//			{
-//				System.out.println(returnVal[i][j]);
-//			}
-//			
-//		}
-//	}
-
 }
